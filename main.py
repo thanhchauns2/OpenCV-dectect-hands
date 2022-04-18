411f94f8b848c1c5d0c2c4718c6fdc211dc306d1
Nguyễn Thanh Châu <69909023+thanhchauns2@users.noreply.github.com>
2022-04-19 03:39:33 +0700

    commit 1

---
+++ main.py
@@ -0,0 +1,15 @@
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    cv2.imshow('Image', img)
+    cv2.waitKey(1)
