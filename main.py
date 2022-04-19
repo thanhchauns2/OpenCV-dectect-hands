4d849d1b020168e15429815bd7bdab70ca63c0a3
Nguyễn Thanh Châu <69909023+thanhchauns2@users.noreply.github.com>
2022-04-19 12:12:42 +0700

    commit 2

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
 
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for hlm in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, hlm, mpHands.HAND_CONNECTIONS)
+
 
     cv2.imshow('Image', img)
     cv2.waitKey(1)
